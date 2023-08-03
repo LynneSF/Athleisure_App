9de28200c3c7a4cc07a1c0c88f3da91cbc6dfad4
snow <139533194+LynneSF@users.noreply.github.com>
2023-08-03 15:03:28 +0100

    Update athleisure_app.py

--- athleisure_app.py
+++ athleisure_app.py
@@ -7,11 +7,17 @@ from urllib.error import URLError
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 streamlit.header("View Our color List")
+
 #Snowflake-related functions
 def get_color_list():
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select sweatsuit_color_or_style from upsell_mapping")
        return my_cur.fetchall()
 
+#import colors
+my_color_list = my_cur
+
+#add a pick list
+colors_to_show = my_color_list.loc[fruits_selected]
 
 
