95a051bd46032e7a82df48f5ecc00807d36aec12
snow <139533194+LynneSF@users.noreply.github.com>
2023-08-03 15:07:09 +0100

    Update athleisure_app.py

--- athleisure_app.py
+++ athleisure_app.py
@@ -10,6 +10,7 @@ streamlit.header("View Our color List")
 
 #Snowflake-related functions
 def get_color_list():
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   with my_cnx.cursor() as my_cur:
        my_cur.execute("select sweatsuit_color_or_style from upsell_mapping")
        return my_cur.fetchall()
